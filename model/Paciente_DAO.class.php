41d1a4cfeb646d48c562efd53590bb828815d736
cbcarlos07 <cbcarlos08@gmail.com>
2016-08-09 08:07:20 -0400

    Atualizacao
    .

--- model/Paciente_DAO.class.php
+++ model/Paciente_DAO.class.php
@@ -368,7 +368,7 @@ class Paciente_DAO{
                    if(isset($row['MEDIA'])){
                        $media = $row['MEDIA'];
                    }else{
-                       $media = '';
+                       $media = 'AGURDANDO ATENDIMENTO';
                    }
                    
                    if(isset($row['DS_SENHA'])){
